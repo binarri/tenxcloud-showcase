fd4208ebde7ae71e5bd4a4e2704236242b90ce70
杜盼盼 <karl2009@foxmail.com>
2018-05-29 14:57:51 +0800

    tenxcloud-eureka

---
+++ tenxcloud-eureka/src/main/java/com/wasu/tenxcloudeureka/TenxcloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.wasu.tenxcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenxcloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenxcloudEurekaApplication.class, args);
+	}
+}
