ac306c403a452e929defcfd3943cd27e4b2c5089
杜盼盼 <karl2009@foxmail.com>
2018-05-30 14:51:17 +0800

    Update

--- tenxcloud-eureka/src/main/java/com/wasu/tenxcloudeureka/TenxcloudEurekaApplication.java
+++ tenxcloud-eureka/src/main/java/com/wasu/tenxcloudeureka/TenxcloudEurekaApplication.java
@@ -2,11 +2,14 @@ package com.wasu.tenxcloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class TenxcloudEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TenxcloudEurekaApplication.class, args);
 	}
+
 }
